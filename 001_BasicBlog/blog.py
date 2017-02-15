1171a6c94eca839eeace129a3e73969ab05f08d3
MasahiroOKUBO <okubo.masahiro@okubo-tech.com>
2017-02-15 09:28:13 +0900

    mod

--- 001_BasicBlog/blog.py
+++ 001_BasicBlog/blog.py
@@ -5,63 +5,74 @@ from string import letters
 import webapp2
 import jinja2
 
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 
+'''
+ -----------------------
+ jinja stuff
+ -----------------------
+'''
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
-def render_str(template, **params):
+def jinja_render_template(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
-class BlogHandler(webapp2.RequestHandler):
+'''
+ -----------------------
+ misc
+ -----------------------
+'''
+
+
+'''
+ -----------------------
+ data stuff
+ -----------------------
+'''
+def posts_key(namespace ='default'):
+    return ndb.Key('posts', namespace)
+
+def render_post(response, post):
+  response.out.write('<b>' + post.subject + '</b><br>')
+  response.out.write(post.content)
+
+class Post(ndb.Model):
+    subject = ndb.StringProperty(required=True)
+    content = ndb.TextProperty(required=True)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    last_modified = ndb.DateTimeProperty(auto_now=True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return jinja_render_str("part-post.html", post=self)
+
+'''
+ -----------------------
+ handler
+ -----------------------
+'''
+class BaseHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
-        return render_str(template, **params)
+        return jinja_render_template(template, **params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-def render_post(response, post):
-    response.out.write('<b>' + post.subject + '</b><br>')
-    response.out.write(post.content)
-
-class MainPage(BlogHandler):
+class MainPage(BaseHandler):
   def get(self):
-      self.response.headers['Content-Type'] = 'text/plain'
-      visits = self.request.cookies.get('visits', 0)
-      if visits.isdigit():
-          visits = int(visits) + 1
-      else:
-          visits = 0
-      self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits)
+      self.render('page-top.html')
 
-      self.write("Hello, Udacity! You've been visits %s times" % visits)
-
-##### blog stuff
-
-def blog_key(name = 'default'):
-    return db.Key.from_path('blogs', name)
-
-class Post(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_modified = db.DateTimeProperty(auto_now = True)
-
-    def render(self):
-        self._render_text = self.content.replace('\n', '<br>')
-        return render_str("post.html", p = self)
-
-class BlogFront(BlogHandler):
+class BlogFront(BaseHandler):
     def get(self):
         posts = db.GqlQuery("select * from Post order by created desc limit 10")
         self.render('front.html', posts = posts)
 
-class PostPage(BlogHandler):
+class PostPage(BaseHandler):
     def get(self, post_id):
         key = db.Key.from_path('Post', int(post_id), parent=blog_key())
         post = db.get(key)
@@ -72,7 +83,7 @@ class PostPage(BlogHandler):
 
         self.render("permalink.html", post = post)
 
-class NewPost(BlogHandler):
+class NewPost(BaseHandler):
     def get(self):
         self.render("newpost.html")
 
@@ -91,7 +102,7 @@ class NewPost(BlogHandler):
 
 
 ###### Unit 2 HW's
-class Rot13(BlogHandler):
+class Rot13(BaseHandler):
     def get(self):
         self.render('rot13-form.html')
 
@@ -116,7 +127,7 @@ EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
 def valid_email(email):
     return not email or EMAIL_RE.match(email)
 
-class Signup(BlogHandler):
+class Signup(BaseHandler):
 
     def get(self):
         self.render("signup-form.html")
@@ -151,7 +162,7 @@ class Signup(BlogHandler):
         else:
             self.redirect('/unit2/welcome?username=' + username)
 
-class Welcome(BlogHandler):
+class Welcome(BaseHandler):
     def get(self):
         username = self.request.get('username')
         if valid_username(username):
