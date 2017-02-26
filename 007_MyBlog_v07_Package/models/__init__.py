460c1cf0d3102240c020f97305a9cdbf4f07b55d
MasahiroOKUBO <okubo.masahiro@okubo-tech.com>
2017-02-26 09:00:23 +0900

    we

---
+++ 007_MyBlog_v07_Package/models/__init__.py
@@ -0,0 +1,4 @@
+from User import User, users_key
+from Post import Post, posts_key, Vote, votes_key, Comment, comments_key
+# from Vote import Vote, votes_key
+# from Comment import Comment, comments_key
